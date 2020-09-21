b9040392525500c0aaa697e7a0434dc23f38e09e
Neil Malu <neilmmalu@gmail.com>
2020-09-21 12:56:09 -0400

    Done with comments

--- HW2/HW3.cpp
+++ HW2/HW3.cpp
@@ -1,3 +1,5 @@
+//HW3 Name: Neil Malu
+//SUID: 635692900 NetID: nemalu
 //HW3:  Implement DFS and BFS as described in class.
 //Due:  11:50PM, Wednesday, September 23.
 //You should not add more functions.
@@ -11,29 +13,31 @@ using namespace std;
 
 class pos {
 public:
-	//your code.
-	//At each position, among many things, you need to decide how to move to a new position.
 	int y_coord; //y_coord goes from 1-8
 	char letter;  //Letter goes from 'a' to 'h'
 
-	bool visited = false;
-	bool inQueue = false;
-	pair<char, int> parent;
+	bool visited = false; //true if a particular position is visited
+	bool inQueue = false; //true if a particular position is already in the BFS queue
+	pair<char, int> parent; //parent position of current position - used for printing path
 
-	vector<int> x_add = { -1, -2, -2, -1, 1, 2, 2, 1 };
-	vector<int> y_add = { -2, -1, 1, 2, 2, 1, -1, -2 };
+	vector<int> x_add = { -1, -2, -2, -1, 1, 2, 2, 1 }; //Order of x-axis neighbor calculation
+	vector<int> y_add = { -2, -1, 1, 2, 2, 1, -1, -2 }; //Order of y-axis neighbor calculation
 
-	pos() { pos('a', 0); }
+	pos() { pos('a', 0); } //Overrided default constructor
 
+	//x coord is the letter
+	//y coord is the number
 	pos(char x, int y) {
 		this->letter = x;
 		this->y_coord = y;
 	}
 
+	//Utility print function
 	void print() {
 		cout << "(" << letter << ", " << y_coord << ")";
 	}
 
+	//Checks if pair of x, y values are valid position on the board
 	bool isValid(char x, int y) {
 		if (x < 'a' || x > 'h') return false;
 		if (y < 1 || y > 8) return false;
@@ -41,6 +45,7 @@ public:
 		return true;
 	}
 
+	//Returns a list of all valid neighbors of a pos, sorted clockwise from bottom left to bottom right
 	vector<pair<char, int>> allValidPos() {
 		vector<pair<char, int>> result;
 		for (int i = 0; i < x_add.size(); ++i) {
@@ -97,8 +102,15 @@ int main() {
 		for (int j = 0; j < board.size(); ++j)
 			board[i][j] = pos(j + 'a', 8 - i);
 
+	//Set up stuff for recursive DFS
+	//Mark start pos as visited and add to stack
 	stack<pair<int, int>> Stack_r;
-	//reset board and some initial work if you see needed.
+	
+
+	int x = start_pos.first - 'a';
+	int y = 8 - start_pos.second;
+	board[y][x].visited = true;
+	Stack_r.push(start_pos);
 	DFS_r(board, Stack_r, start_pos, end_pos);//recursive DFS
 
 	return 0;
@@ -106,19 +118,21 @@ int main() {
 
 
 void BFS(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, int> end_pos) {
-	//Your code
-	//Note that if you can reach the end_pos, print the path otherwise print "No path available!".
 
+	//Push the start position to the queue
 	queue<pair<char, int>> Q;
 	Q.push(start_pos);
 	while (!Q.empty()) {
 		auto p = Q.front();
 		Q.pop();
 
+		//Calculations to convert from pair to pos on board
 		int x = p.first - 'a';
 		int y = 8 - p.second;
 
+		//exit condition - if the end position is found
 		if (p.first == end_pos.first && p.second == end_pos.second) {
+			//Keep printing while x and y values don't correspond to the start position
 			while (!(x == start_pos.first - 'a' && y == 8 - start_pos.second)) {
 				board[y][x].print();
 				cout << " <-- ";
@@ -131,14 +145,20 @@ void BFS(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, int>
 			return;
 		}
 
+		//If the current position is not visited
 		if (!board[y][x].visited) {
+			//Mark the current position as visited
 			board[y][x].visited = true;
+			//find all the neighbors of the current position
 			vector<pair<char, int>> neighbors = board[y][x].allValidPos();
 			for (auto n : neighbors) {
 				int newX = n.first - 'a';
 				int newY = 8 - n.second;
+				//If the neighbor is not in queue or not visited
 				if (!board[newY][newX].visited && !board[newY][newX].inQueue) {
+					//Add the neighbor to the queue
 					board[newY][newX].inQueue = true;
+					//Set the parent of the neigbor to the current position
 					board[newY][newX].parent = p;
 					Q.push(n);
 				}
@@ -150,15 +170,59 @@ void BFS(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, int>
 	cout << "No path available!" << endl;
 }
 void DFS_r(vector<vector<pos>>& board, stack<pair<int, int>>& Stack_r, pair<char, int> start_pos, pair <char, int> end_pos) {
-	//Your code
-	//Note that if you can reach the end_pos, print the path otherwise print "No path available!".
+	//Initially, start position is already in the stack
+	while (!Stack_r.empty()) {
+		auto p = Stack_r.top();
 
+		//Calculations to convert from pair to pos on board
+		int x = p.first - 'a';
+		int y = 8 - p.second;
 
+		//exit condition - if the end position is found
+		if (p.first == end_pos.first && p.second == end_pos.second) {
+			//Keep printing while x and y values don't correspond to the start position
+			while (!(x == start_pos.first - 'a' && y == 8 - start_pos.second)) {
+				board[y][x].print();
+				cout << " <-- ";
+				int newX = board[y][x].parent.first - 'a';
+				int newY = 8 - board[y][x].parent.second;
+				x = newX;
+				y = newY;
+			}
+			board[y][x].print();
+			Stack_r.pop();
+			return;
+		}
+
+		//find all the neighbors of the current position
+		vector<pair<char, int>> neighbors = board[y][x].allValidPos();
+		for (auto n : neighbors) {
+			int newX = n.first - 'a';
+			int newY = 8 - n.second;
+			//If the neighbor is not visited
+			if (!board[newY][newX].visited) {
+				//Mark the neighbor as visited
+				board[newY][newX].visited = true;
+				//Set the parent of the neighbor as the current pos
+				board[newY][newX].parent = p;
+				//Push the neighbor to the stack
+				Stack_r.push(n);
+				//Recursively call DFS
+				DFS_r(board, Stack_r, start_pos, end_pos);
+			}
+		}
+		//Pop from the stack once pos is visited
+		if(!Stack_r.empty())Stack_r.pop();
+	}
+
+	int endX = end_pos.first - 'a';
+	int endY = 8 - end_pos.second;
+
+	if(!board[endY][endX].visited) cout << "No path available!" << endl;
 }
 void DFS_nr(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, int> end_pos) {
-	//Your code
-	//Note that if you can reach the end_pos, print the path otherwise print "No path available!".
-	
+
+	//Push the start position to the queue	
 	stack<pair<char, int>> S;
 	S.push(start_pos);
 
@@ -166,10 +230,13 @@ void DFS_nr(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, i
 		auto p = S.top();
 		S.pop();
 
+		//Calculations to convert from pair to pos on board
 		int x = p.first - 'a';
 		int y = 8 - p.second;
 
+		//exit condition - if the end position is found
 		if (p.first == end_pos.first && p.second == end_pos.second) {
+			//Keep printing while x and y values don't correspond to the start position
 			while (!(x == start_pos.first - 'a' && y == 8 - start_pos.second)) {
 				board[y][x].print();
 				cout << " <-- ";
@@ -182,15 +249,21 @@ void DFS_nr(vector<vector<pos>>& board, pair<char, int> start_pos, pair <char, i
 			return;
 		}
 
+		//If the current position is not visited
 		if (!board[y][x].visited) {
+			//Mark the current position as visited
 			board[y][x].visited = true;
 			vector<pair<char, int>> neighbors = board[y][x].allValidPos();
+			//Add the neigbors to the stack in reverse order, so next valid position is on top
 			for (int i = neighbors.size() - 1; i >= 0; --i) {
 				auto n = neighbors[i];
 				int newX = n.first - 'a';
 				int newY = 8 - n.second;
+				//If the neighbor is not visited
 				if (!board[newY][newX].visited) {
+					//Set the parent of neighbor to the current pos
 					board[newY][newX].parent = p;
+					//Add the neighbor to the queue
 					S.push(n);
 				}
 			}
